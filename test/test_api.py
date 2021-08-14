f99d331a69e798068430d78d94c32ff7ee106712
Struan Duncan-Wilson <struan@duncan-wilson.co.uk>
2021-08-14 17:08:07 +0100

    Add challenge ratings API test

--- test/test_api.py
+++ test/test_api.py
@@ -24,8 +24,29 @@ def test_environments_gives_correct_list(client):
 
 
 def test_size_gives_json_with_proper_mimetype(client):
+    response = client.get("/api/sizes")
+    assert response.status_code == 200
+    assert response.content_type == 'application/json'
+
+
+def test_size_gives_json_with_sizes(client):
     expected = ["Tiny", "Small", "Medium", "Large", "Huge", "Gargantuan"]
     response = client.get("/api/sizes")
+    received = response.get_json()
+
+    assert received == expected
+
+
+def test_cr_list_gives_json_with_proper_mimetype(client):
+    response = client.get("/api/crs")
+    assert response.status_code == 200
+    assert response.content_type == 'application/json'
+
+
+def test_cr_list_gives_correct_list(client):
+    expected = ['0', '1/8', '1/4', '1/2', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12',
+                '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '30']
+    response = client.get("/api/crs")
     received = response.get_json()
 
     assert received == expected
--- test/test_api_functions.py
+++ test/test_api_functions.py
@@ -1,4 +1,4 @@
-from ktc.api import get_list_of_environments, get_list_of_sizes
+from ktc.api import get_list_of_environments, get_list_of_sizes, get_list_of_monster_types, get_list_of_challenge_ratings
 
 
 def test_environment_list_returns_list():
@@ -23,4 +23,32 @@ def test_size_list_returns_list():
 def test_size_list_returns_unique_list_of_sizes():
     expected = ["Tiny", "Small", "Medium", "Large", "Huge", "Gargantuan"]
     actual = get_list_of_sizes()
+    assert expected == actual
+
+
+def test_type_list_returns_list():
+    expected = type(list())
+    actual = type(get_list_of_monster_types())
+    assert expected == actual
+
+
+def test_type_list_returns_unique_list_of_monster_types():
+    expected = ["Aberration", "Beast", "Celestial", "Construct", "Dragon", "Elemental",
+                "Fey", "Fiend", "Giant", "Humanoid", "Monstrosity", "Ooze", "Plant", "Undead"]
+    actual = get_list_of_monster_types()
+
+    assert expected == actual
+
+
+def test_cr_list_returns_list():
+    expected = type(list())
+    actual = type(get_list_of_challenge_ratings())
+    assert expected == actual
+
+
+def test_cr_list_returns_unique_list_of_challenge_ratings():
+    expected = ['0', '1/8', '1/4', '1/2', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12',
+                '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '30']
+    actual = get_list_of_challenge_ratings()
+
     assert expected == actual
