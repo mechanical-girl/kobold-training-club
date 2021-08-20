9966cd6c2857f72aa9ed9f1af9c597373ae9663f
Struan Duncan-Wilson <struan@duncan-wilson.co.uk>
2021-08-20 18:09:38 +0100

    Add shebang

--- runner.sh
+++ runner.sh
@@ -1,4 +1,5 @@
+#!/bin/bash
 npm test
-browserify ktc/static/js/main.js -o ktc/static/js/bundle.js 
+browserify ktc/static/js/main.js -o ktc/static/js/bundle.js
 coverage run -m pytest test/*.py -vv
 mypy ktc/*.py
