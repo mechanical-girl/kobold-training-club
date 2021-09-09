057ca9f2d39ba3bbb36a2e78897bafcc53240b67
Struan Duncan-Wilson <struan@duncan-wilson.co.uk>
2021-09-09 15:07:41 +0100

    Fix #79

--- ktc/api.py
+++ ktc/api.py
@@ -307,7 +307,7 @@ def get_list_of_monsters(parameters: Dict) -> Dict[str, List[List[str]]]:
 
     with contextlib.closing(sqlite3.connect(db_location)) as conn:
         c = conn.cursor()
-        conn.set_trace_callback(print)
+        # conn.set_trace_callback(print)
 
         if query_arguments == []:
             c.execute(query_string)
