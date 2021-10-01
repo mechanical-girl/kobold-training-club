988d98aa719d829f0d60f8c78cb576ba9dbb03d7
Struan Duncan-Wilson <struan@duncan-wilson.co.uk>
2021-10-01 10:29:37 +0100

    Fix tests

--- runner.sh
+++ runner.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 npm test
-browserify ktc/static/js/main.js -o ktc/static/js/bundle.js
+browserify ktc/static/js/js/* -o ktc/static/js/bundle.js
 coverage run -m pytest test/*.py -vv -x
 coverage xml
 codecov -F python -f coverage.xml -t d2d0d3db-98bd-4add-8ab8-782003116ae9
 mypy ktc/*.py
+python pylint ktc/*
