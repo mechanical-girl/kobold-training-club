bf2bff95ce2bf67013d7730d24e97a982fa2c90b
Struan Duncan-Wilson <struan@duncan-wilson.co.uk>
2021-10-01 09:35:28 +0100

    Stash

--- ktc/static/js/bundle.js
+++ ktc/static/js/bundle.js
@@ -566,4 +566,4 @@ var toggleAll = function (clicked_button) {
 
 module.exports = { GetUpdatedValues: GetUpdatedValues, AssociatedId: AssociatedId, getUpdatedChallengeRatings: getUpdatedChallengeRatings, floatify: floatify, sortTable: sortTable, toggleAll: toggleAll }
 
-},{}]},{},[1,3,4,6]);
+},{}]},{},[1,3,4,6,2,5]);
